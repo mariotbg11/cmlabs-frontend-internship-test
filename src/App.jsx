21a9259f6272a397731362ff201acf1787e7a8ba
mariotbg11 <christofell.m@gmail.com>
2024-11-02 16:54:06 +0700

    feat: create hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="font-fredoka max-w-screen-xl mx-auto">
+    <div className="font-fredoka max-w-screen-xl mx-auto px-2">
       <Navbar />
+      <Hero />
     </div>
   );
 }
