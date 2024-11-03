97792cce8b1036d0669ad7d0cc13296f0b3c5669
mariotbg11 <christofell.m@gmail.com>
2024-11-03 10:22:58 +0700

    feat: create card list component

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -1,15 +1,11 @@
-function CardItem() {
+function CardItem({ image, title }) {
   return (
     <div className="card bg-base-100 shadow-xl">
       <figure className="px-10 pt-10">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-          alt="Shoes"
-          className="rounded-xl"
-        />
+        <img src={image} alt="Shoes" className="rounded-xl" />
       </figure>
       <div className="card-body items-center text-center">
-        <h2 className="card-title">Shoes!</h2>
+        <h2 className="card-title">{title}</h2>
       </div>
     </div>
   );
--- src/pages/CategoryFood.jsx
+++ src/pages/CategoryFood.jsx
@@ -1,7 +1,15 @@
+import useFetch from "../hooks/useFetch";
+import CardList from "../components/CardList";
+
 function CategoryFood() {
+  const { data, loading, error } = useFetch(
+    "https://www.themealdb.com/api/json/v1/1/categories.php"
+  );
+
   return (
     <div className="bg-base-100 my-16">
-      <h2 className="text-4xl font-bold">Category Food</h2>
+      <h2 className="text-4xl font-bold mb-14">Category Food</h2>
+      <CardList data={data} loading={loading} error={error} />
     </div>
   );
 }
